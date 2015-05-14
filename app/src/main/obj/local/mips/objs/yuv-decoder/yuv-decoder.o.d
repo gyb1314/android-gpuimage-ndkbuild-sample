427680994d9ea89298aa87ff24e321f1dd0973de
DUBU <dubudev@gmail.com>
2015-05-14 10:39:39 +0900

    first update
    first update

---
+++ app/src/main/obj/local/mips/objs/yuv-decoder/yuv-decoder.o.d
@@ -0,0 +1,16 @@
+obj/local/mips/objs/yuv-decoder/yuv-decoder.o: jni/yuv-decoder.c \
+ /Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/jni.h \
+ /Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/sys/cdefs.h \
+ /Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h \
+ /Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/android/api-level.h \
+ /Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/android/log.h
+
+/Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/jni.h:
+
+/Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/sys/cdefs.h:
+
+/Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/sys/cdefs_elf.h:
+
+/Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/android/api-level.h:
+
+/Users/leejaegil/androidndk/platforms/android-9/arch-mips/usr/include/android/log.h:
